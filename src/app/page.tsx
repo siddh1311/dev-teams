2ec98c45a1d46feef006e45689e0993003e368eb
siddh <siddhpatel131@gmail.com>
2024-09-04 17:08:23 -0700

    cleaned up logged-in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-
-import { useAuthActions } from "@convex-dev/auth/react";
-
 import { UserButton } from "@/features/auth/components/UserButton";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
-      Logged In!
       <UserButton />
     </div>
   );
