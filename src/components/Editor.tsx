a4417278ea44a3c6ddee9a38fbc18a14d2f0faa3
siddh <siddhpatel131@gmail.com>
2024-09-12 22:32:07 -0700

    added editor functionality to insert emojis

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -16,6 +16,7 @@ import "quill/dist/quill.snow.css";
 
 import { Button } from "./ui/button";
 import Hint from "./Hint";
+import { EmojiPopver } from "./EmojiiPopover";
 
 import { cn } from "@/lib/utils";
 
@@ -142,6 +143,11 @@ const Editor = ({
     }
   };
 
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
+  };
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
@@ -160,16 +166,11 @@ const Editor = ({
             </Button>
           </Hint>
 
-          <Hint label="Emoji">
-            <Button
-              disabled={disabled}
-              size="iconSm"
-              variant={"ghost"}
-              onClick={() => {}}
-            >
+          <EmojiPopver onEmojiSelect={onEmojiSelect}>
+            <Button disabled={disabled} size="iconSm" variant={"ghost"}>
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopver>
           {variant === "create" && (
             <Hint label="Image">
               <Button
@@ -222,11 +223,18 @@ const Editor = ({
         </div>
       </div>
 
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Enter</strong> to add a new line
-        </p>
-      </div>
+      {variant === "create" && (
+        <div
+          className={cn(
+            "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
+            !isEmpty && "opacity-100"
+          )}
+        >
+          <p>
+            <strong>Shift + Enter</strong> to add a new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
