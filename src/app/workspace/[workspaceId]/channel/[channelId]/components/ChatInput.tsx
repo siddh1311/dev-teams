beccdb485871472bbb440ff8792caf713ece5b00
siddh <siddhpatel131@gmail.com>
2024-09-13 23:26:55 -0700

    added message submit functionality

--- src/app/workspace/[workspaceId]/channel/[channelId]/components/ChatInput.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/components/ChatInput.tsx
@@ -1,6 +1,13 @@
+import { toast } from "sonner";
+
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useChannelId } from "@/hooks/use-channel-id";
 
 const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
 
@@ -9,14 +16,46 @@ interface ChatInputProps {
 }
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
   const editorRef = useRef<Quill | null>(null);
+
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+  const { mutate: createMessage } = useCreateMessage();
+
+  const handleSubmit = async ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    try {
+      setIsPending(true);
+      console.log({ body, image });
+      await createMessage(
+        { workspaceId, channelId, body },
+        { throwError: true }
+      );
+
+      // dirty trick to reset editor after submit. forces the editor to rerender
+      setEditorKey((prev) => prev + 1);
+    } catch (error) {
+      toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+    }
+  };
   return (
     <div className="px-5 w-full">
       <Editor
+        key={editorKey}
         variant="create"
         placeholder={placeholder}
-        onSubmit={() => {}}
-        disabled={false}
+        onSubmit={handleSubmit}
+        disabled={isPending}
         innerRef={editorRef}
       />
     </div>
---
+++ src/features/messages/api/use-create-message.ts
@@ -0,0 +1,81 @@
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useCallback, useMemo, useState } from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = {
+  body: string;
+  workspaceId: Id<"workspaces">;
+  image?: Id<"_storage">;
+  channelId?: Id<"channels">;
+  parentMessageId?: Id<"messages">;
+  //   TODO: add conversationId
+};
+type ResponseType = Id<"messages"> | null;
+
+type Optons = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+/**
+ * A hook to create a workspace.
+ *
+ * @returns
+ * - `mutate`: The mutation function to create the workspace.
+ * - `data`: The response from the mutation.
+ * - `error`: An error if the mutation fails.
+ * - `isPending`, `isSuccess`, `isError`, `isSettled`: States of the mutation.
+ */
+export const useCreateMessage = () => {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const [status, setStatus] = useState<
+    "pending" | "success" | "error" | "settled" | null
+  >(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.messages.create);
+
+  const mutate = useCallback(
+    async (values: RequestType, options?: Optons) => {
+      try {
+        // Resetting all the states
+        setData(null);
+        setError(null);
+        setStatus("pending");
+
+        const response = await mutation(values);
+        options?.onSuccess?.(response);
+
+        return response;
+      } catch (error) {
+        setStatus("error");
+        options?.onError?.(error as Error);
+
+        if (options?.throwError) throw error;
+      } finally {
+        setStatus("settled");
+        options?.onSettled?.();
+      }
+    },
+    [mutation]
+  );
+
+  return {
+    mutate,
+    data,
+    error,
+    isPending,
+    isSuccess,
+    isError,
+    isSettled,
+  };
+};
