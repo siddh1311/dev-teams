4a10c4f7e893aace759de020cc3e29b4502011fb
siddh <siddhpatel131@gmail.com>
2024-09-22 13:38:38 -0700

    added loader in main root landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-
-import { UserButton } from "@/features/auth/components/UserButton";
-import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
-
-import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
+
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
 export default function Home() {
   const router = useRouter();
@@ -26,5 +25,9 @@ export default function Home() {
     }
   }, [workspaceId, isLoading, open, setOpen, router]);
 
-  return <div></div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
+    </div>
+  );
 }
